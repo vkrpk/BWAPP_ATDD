e0a15c2574d27675b46295bab11dff0c9ad60439
vkrpk <victor.krupka@orange.fr>
2024-03-05 10:37:31 +0100

    first commit

---
+++ Bwapp/ComponentHelper/NavigationHelper.cs
@@ -0,0 +1,32 @@
+﻿using BugzillaWebDriver.BaseClasses;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugzillaWebDriver.ComponentHelper
+{
+    public class NavigationHelper
+    {
+        public static void NavigateToUrl(string url)
+        {
+            ObjectRepository.Driver.Navigate().GoToUrl(url);
+        }
+
+        public static void NavigateToHomePage()
+        {
+            ObjectRepository.Driver.Navigate().GoToUrl(ObjectRepository.Config.GetWebsite());
+        }
+
+        public static void Logout()
+        {
+            if (PageHelper.GetPageTitle() != "bWAPP - Login")
+            {
+                LinkHelper.ClickLink(By.LinkText("Logout"));
+                AlertHelper.Accept();
+                Task.Delay(100).Wait();
+            }
+        }
+    }
+}
